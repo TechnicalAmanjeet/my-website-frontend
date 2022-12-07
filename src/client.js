d3b1f1aa48db6c7952ac8ad00d847f86a69b590d
Amanjeet kumar <techamanjeet1@gmail.com>
2022-12-08 00:11:02 +0530

    working fine till now

--- src/client.js
+++ src/client.js
@@ -1,11 +1,19 @@
+import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
-import sanityClient from '@sanity/client'
+
+// export const client = sanityClient({
+//     projectId: process.env.MY_WEBSITE_SANITY_PROJECT_ID,
+//     dataset: process.env.MY_WEBSITE_SANITY_DATASET,
+//     apiVersion: process.env.MY_WEBSITE_SANITY_API_VERSION,
+//     token: process.env.MY_WEBSITE_SANITY_TOKEN,
+//     useCdn: process.env.MY_WEBSITE_SANITY_USE_CDN,
+// });
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion:'2021-12-21',
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    projectId: "upl574tz",
+    dataset: "production",
+    apiVersion: "2021-12-21",
+    token: "skHd3MoNZdTx30yAJdKP58rmsYLPF7v98nVayj21IATrvhEcH3vurHz43XeZdCeZyKdSieeALsZCEMyGEsTS6927foxtQw5bBXap9RIyFnFLLHOPQi7JzwAkdb9Oe2ToJCSbgQAyCJ307rEI53YDBJpkcx2b9owxCxq7KBU68syHy2XScw5m",
     useCdn: true,
 });
 
